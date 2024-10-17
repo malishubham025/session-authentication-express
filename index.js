6234c13244bf22527acbee34705ebd4a42693646
malishubham025 <malishubham025@gmail.com>
2024-10-18 00:38:19 +0530

    implemented

--- index.js
+++ index.js
@@ -10,15 +10,32 @@ app.use(session({
     resave:false,
     saveUninitialized:false
 }))
-let count=0;
+// let count=0;
 app.get("/",(req,res)=>{
     req.session.count++;
+    // req.x["hello"]=count;
     res.render("index",{count:req.session.count});
 });
-app.post("/login",(req,res)=>{
-    req.session.count++;
-    res.render("index",{count:req.session.count});
-});
+app.get('/login', (req, res) => {
+    // req.session.user = { username: 'JohnDoe', role: 'admin' };  // Set session data
+    console.log(req.query);
+    req.session.user ={username:req.query.hello,password:req.query.pass}
+    res.send('Session data set!');
+  });
+  
+  // Route to access session data
+  app.get('/dashboard', (req, res) => {
+    if (req.session.user) {
+      res.send(`Welcome ${req.session.user.username}, you are logged in as ${req.session.user.password}`);
+    } else {
+      res.send('Please log in first!');
+    }
+  });
+// app.post("/login",(req,res)=>{
+//     // req.session.count++;
+//     // console.log(req.session.x["hello"]);
+//     res.render("index",{count:req.session.count});
+// });
 
 app.listen(3000,()=>{
     console.log("listning...");
